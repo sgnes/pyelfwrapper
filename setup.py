f3b82f25a259d67164956203546d70cb9bcf130e
sgnes <sgnes0514@gmail.com>
2025-01-30 12:04:32 +0800

    update version for 0.0.6

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pyelfwrapper", # Replace with your own username
-    version="0.0.5",
+    version="0.0.6",
     author="Sgnes",
     author_email="sgnes0514@gmai.com",
     description="A wrapper for dwarf format elf file",
